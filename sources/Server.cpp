951b501910d2c0472578487ef6382d54387fb816
Mohamed Ajermoune <majermou@e2r5p15.1337.ma>
2021-12-27 16:08:15 +0100

    fix: revert to 5fb3e3f78

--- sources/Server.cpp
+++ sources/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: abel-mak <abel-mak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:24:54 by abel-mak          #+#    #+#             */
-/*   Updated: 2021/12/22 18:53:36 by abel-mak         ###   ########.fr       */
+/*   Updated: 2021/12/20 18:53:47 by abel-mak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ Server::Server(void) : _bufSize(1024)
 }
 
 Server::Server(const std::vector<ServerData> &data)
-    : _mypoll(data), _bufSize(1024)
+    : _mypool(data), _bufSize(1024)
 {
 }
 
@@ -84,7 +84,7 @@ void Server::run(void)
 	    "1000px;}</style>";
 	while (1)
 	{
-		readyFds = _mypoll.getReadyfds();
+		readyFds = _mypool.getReadyfds();
 		if (readyFds.size() > 0)
 		{
 			i = 0;
@@ -113,11 +113,11 @@ void Server::run(void)
 				if (checkCrlf(_rawRequest[readyFds[i]], readyFds[i]) == true)
 				{
 					response = (handleRequest(_rawRequest[readyFds[i]],
-					                          _mypoll.getData()))
+					                          _mypool.getData()))
 					               .response;
 					send(readyFds[i], response.c_str(), response.length(), 0);
 					_rawRequest.erase(readyFds[i]);
-					_mypoll.clearActiveFd(readyFds[i]);
+					_mypool.clearActiveFd(readyFds[i]);
 					close(readyFds[i]);
 				}
 				i++;
