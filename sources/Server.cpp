d62a89e2a8379f0758199f272d94b4fcadfb86fd
Abdelali El Makkioui <abdelmkey19973@gmail.com>
2021-12-31 09:47:29 +0100

    fix: server

--- sources/Server.cpp
+++ sources/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: abel-mak <abel-mak@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 13:24:54 by abel-mak          #+#    #+#             */
-/*   Updated: 2021/12/30 17:39:44 by abel-mak         ###   ########.fr       */
+/*   Updated: 2021/12/31 09:46:37 by abel-mak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,53 +65,40 @@ void Server::sendResponse(void)
 	std::vector<int> wReadyFds = _mypoll.getWriteReadyFds();
 	int i;
 	int tmp;
-	int chunk;
 
 	i = 0;
 	while (i < wReadyFds.size())
 	{
-		// std::cout << i << " wReadyFds: " << wReadyFds.size() << std::endl;
 		if (_responseData.find(wReadyFds[i]) != _responseData.end())
 		{
 			tmp =
 			    send(wReadyFds[i], _responseData[wReadyFds[i]].response.data(),
 			         _responseData[wReadyFds[i]].response.size(), 0);
 			signal(SIGPIPE, SIG_IGN);
-			if (tmp < 0)
+			if (tmp >= 0)
 			{
-				// std::cout << "-------B-------" << std::endl;
-				// std::cout << strerror(errno) << std::endl;
-				// std::cout << wReadyFds[i] << std::endl;
-				// std::cout << "-------E-------" << std::endl;
-			}
-			else if (tmp == _responseData[wReadyFds[i]].response.length())
-			{
-				if (_responseData[wReadyFds[i]].connection == false)
+				if (tmp == _responseData[wReadyFds[i]].response.length())
 				{
-					_mypoll.clearActiveFd(wReadyFds[i]);
-					// std::cout << "-------B-------" << std::endl;
-					// std::cout << "sendResp closed" << std::endl;
-					close(wReadyFds[i]);
-					// std::cout << "-------E-------" << std::endl;
+					if (_responseData[wReadyFds[i]].connection == false)
+					{
+						_mypoll.clearActiveFd(wReadyFds[i]);
+						close(wReadyFds[i]);
+					}
+					// std::cout << "cleared writeactive and erased" <<
+					// std::endl;
+					_mypoll.clearWriteActiveFd(wReadyFds[i]);
+					_responseData.erase(wReadyFds[i]);
 				}
-				//	std::cout << "-------B-------" << std::endl;
-				//	std::cout << "cleared writeactive and erased" << std::endl;
-				_mypoll.clearWriteActiveFd(wReadyFds[i]);
-				_responseData.erase(wReadyFds[i]);
-				// std::cout << "-------E-------" << std::endl;
+				else if (tmp != _responseData[wReadyFds[i]].response.length())
+				{
+					_responseData[wReadyFds[i]].response =
+					    _responseData[wReadyFds[i]].response.substr(tmp);
+				}
 			}
-			else if (tmp != _responseData[wReadyFds[i]].response.length())
+			else if (errno != EPIPE)
 			{
-				//	std::cout << tmp << " *************** "
-				//	          << _responseData[wReadyFds[i]].response.length()
-				//	          << std::endl;
-				_responseData[wReadyFds[i]].response =
-				    _responseData[wReadyFds[i]].response.substr(tmp);
+				std::cout << strerror(errno) << std::endl;
 			}
-			// std::cout << tmp << " << tmp | _resdata >> "
-			// std::cout << "_resdata"
-			//          << _responseData[wReadyFds[i]].response.length()
-			//          << std::endl;
 		}
 		i++;
 	}
@@ -128,23 +115,8 @@ void Server::run(void)
 	int i;
 	int tmp;
 	char buf[_bufSize];
-	std::string response;
 	struct Ret hr;
-	std::string str = "\r\n\r\n";
 
-	response =
-	    "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
-	    "\n"
-	    "<div style=\"font-family: monospace; white-space: pre;\">"
-	    " _    _  _____ ______  _____  _____ ______  _   _ <br>"
-	    "| |  | ||  ___|| ___ \\/  ___||  ___|| ___ \\| | | |<br>"
-	    "| |  | || |__  | |_/ /\\ `--. | |__  | |_/ /| | | |<br>"
-	    "| |/\\| ||  __| | ___ \\ `--. \\|  __| |    / | | | |<br>"
-	    "\\  /\\  /| |___ | |_/ //\\__/ /| |___ | |\\ \\ \\ \\_/ /<br>"
-	    " \\/  \\/ \\____/ \\____/ \\____/ \\____/ \\_| \\_| \\___/ <br>"
-	    "</div>"
-	    "<style>body{margin-left: auto;margin-right: auto;max-width: "
-	    "1000px;}</style>";
 	while (1)
 	{
 		readyFds = _mypoll.getReadyfds();
@@ -179,19 +151,13 @@ void Server::run(void)
 						{
 							_responseData[readyFds[i]] = hr;
 							_mypoll.setWriteActiveFd(readyFds[i]);
-							// response = hr.response;
-							// send(readyFds[i], response.c_str(),
-							//     response.length(), 0);
 						}
 						if (tmp == 0)
 						{
 							_mypoll.clearActiveFd(readyFds[i]);
 							_mypoll.clearWriteActiveFd(readyFds[i]);
 							_mypoll.clearWriteFd(readyFds[i]);
-							// std::cout << "-------B-------" << std::endl;
-							// std::cout << "other closed" << std::endl;
 							close(readyFds[i]);
-							// std::cout << "-------E-------" << std::endl;
 						}
 						_rawRequest.erase(readyFds[i]);
 					}
